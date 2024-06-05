1c6560fe30662f651b6d345dcf4b21499dc75a92
Wasin <wasin1026@gmail.com>
2024-06-05 22:06:08 +0700

    Exercise #5 : Student Grade Calculation

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,20 @@
 function calculateStudentGrade(score) {
   // Start coding here
+  let grade;
+  if (score >= 0 && score <= 59) {
+    grade = "F";
+  } else if (score >= 60 && score <= 69) {
+    grade = "D";
+  } else if (score >= 70 && score <= 79) {
+    grade = "C";
+  } else if (score >= 80 && score <= 89) {
+    grade = "B";
+  } else if (score >= 90 && score <= 100) {
+    grade = "A";
+  } else {
+    grade = "F";
+  }
+  return grade;
 }
 
 // Example case
